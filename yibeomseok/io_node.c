d6b1919c07a0c4337baefc750be43e3585f33673
yibeomseok <vv99911@gmail.com>
2022-12-18 20:13:24 +0900

    [FIX] 파일 크기 수정

--- yibeomseok/io_node.c
+++ yibeomseok/io_node.c
@@ -13,15 +13,13 @@
 
 #define KEY_NUM_1 103861
 #define KEY_NUM_2 103862
+#define MTYPE 1
 
 struct msgbuf
 {
   long mtype;
   int mdata[256];
 };
-
-void handler(int signo) {
-}
 
 /* 어떤 message queue에서 receive할 것인지 인자로 줘야 함 */
 int main(int argc, char *argv[])
@@ -38,7 +36,7 @@ int main(int argc, char *argv[])
   char *recv_file_name = argv[3];
   int fd, len;
   struct msgbuf msg;
-  msg.mtype = 1;
+  msg.mtype = MTYPE;
   sigset_t set;
 
   if ((fd = open(recv_file_name, O_CREAT | O_RDWR, 0644)) == -1)
@@ -46,8 +44,9 @@ int main(int argc, char *argv[])
     perror("open");
     exit(1);
   }
+
   int i;
-  for (i = 0; i < k_size * 1024; i++)
+  for (i = 0; i < k_size * 1024 / 2; i++)
   {
     int temp[1] = {0};
     write(fd, temp, sizeof(int));
@@ -58,11 +57,13 @@ int main(int argc, char *argv[])
   while (1)
   {
     len = msgrcv(msgq_id, &msg, 256 * sizeof(int), 1, 0);
-    if(len == 0) continue;
+    if (len == 0)
+      continue;
     io_write(fd, msg.mdata, len);
     w_cnt++;
     printf("io_node count = %d\n", w_cnt);
-    if(w_cnt == last_cnt){
+    if (w_cnt == last_cnt)
+    {
       msgctl(msgq_id, IPC_RMID, NULL);
       break;
     }
@@ -73,15 +74,40 @@ int main(int argc, char *argv[])
   return 0;
 }
 
+// int io_write(int fd, int *data, int byte_size)
+// {
+//   printf("io에서 256개 받고 쓰기 시작할 때 첫 정수 = %d\n\n", data[0]);
+//   int d_cnt = byte_size / 4;
+//   int i;
+//   int buf[1];
+//   for (i = 0; i < d_cnt; i++)
+//   {
+//     buf[0] = data[i];
+//     lseek(fd, data[i] * 4, SEEK_SET);
+//     write(fd, buf, sizeof(int));
+//   }
+// }
+
 int io_write(int fd, int *data, int byte_size)
 {
   int d_cnt = byte_size / 4;
   int i;
   int buf[1];
+  int unit = data[0] - (data[0] % 1024);
+  int sub = (unit / 1024);
+  int st;
+  if (sub == 0)
+    st = data[0];
+  else
+    st = (unit / sub * (sub / 2)) + (data[0] % 4);
+  off_t start = lseek(fd, st * 4, SEEK_SET);
+  printf("io에서 256개 받고 쓰기 시작할 때 첫 정수 = %d\nstart = %d\n", data[0], (int)st);
+  printf("io에 offset = %d위치에 쓰며 될것임\n\n", (int)start);
+  
   for (i = 0; i < d_cnt; i++)
   {
     buf[0] = data[i];
-    lseek(fd, data[i] * 4, SEEK_SET);
     write(fd, buf, sizeof(int));
+    start = lseek(fd, 3 * sizeof(int), SEEK_CUR);
   }
 }
